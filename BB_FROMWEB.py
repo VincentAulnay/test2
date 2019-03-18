43cd562ac113ea02120de850f5ee2d6c6ecde872
VincentAulnay <48682002+VincentAulnay@users.noreply.github.com>
2019-03-18 14:29:32 +0100

    Update BB_FROMWEB.py

--- BB_FROMWEB.py
+++ BB_FROMWEB.py
@@ -9,9 +9,9 @@ print ('▀▄▀▄▀▄ STOP AIRBNB ▄▀▄▀▄▀')
 
 #-----EXCEL RESULT OPEN AND READ-----
 
-#wbx = load_workbook(path_RESULT.filename)
-#ws = wbx.active
-#ws.cell(row=1, column=1).value = 'name_mois'
-#wbx.save(path_RESULT.filename)
+wbx = load_workbook(path_RESULT.filename)
+ws = wbx.active
+ws.cell(row=1, column=1).value = 'name_mois'
+wbx.save(path_RESULT.filename)
 
 print (toto)
