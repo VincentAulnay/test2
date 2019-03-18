9e0df59c48d0cb0ab8b1e9865dcb5f929b1895a3
VincentAulnay <48682002+VincentAulnay@users.noreply.github.com>
2019-03-18 14:06:21 +0100

    Add files via upload

---
+++ BB_FROMWEB.py
@@ -0,0 +1,17 @@
+
+from urllib.request import urlopen
+from openpyxl import load_workbook
+
+
+print ('▀▄▀▄▀▄ STOP AIRBNB ▄▀▄▀▄▀')
+
+
+
+#-----EXCEL RESULT OPEN AND READ-----
+
+#wbx = load_workbook(path_RESULT.filename)
+#ws = wbx.active
+#ws.cell(row=1, column=1).value = 'name_mois'
+#wbx.save(path_RESULT.filename)
+
+print (toto)
