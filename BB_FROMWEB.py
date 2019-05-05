9cea0a48ef5e8f1f2283e5061abd6a67ea88022e
VincentAulnay <48682002+VincentAulnay@users.noreply.github.com>
2019-05-05 17:06:27 +0200

    Update BB_FROMWEB.py

--- BB_FROMWEB.py
+++ BB_FROMWEB.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-#import xlwt
+import xlwt
 import xlrd
 import time
 from xlutils.copy import copy
