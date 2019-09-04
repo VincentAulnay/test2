099170e24ab12d4a75138cd4c6980d366a0e7bbd
VincentAulnay <48682002+VincentAulnay@users.noreply.github.com>
2019-09-04 21:31:19 +0200

    Update BB_FROMWEB.py

--- BB_FROMWEB.py
+++ BB_FROMWEB.py
@@ -312,6 +312,7 @@ def A_Statu_day2(date,c_write,page,j,g,ResAirbnb,new_mo,MNday,ONCOM):
 		except:
 			break
 	ws.cell(row=j, column=c_write+1).value = str(li)
+	print(li)
 	try:
 		if len(li)>0:
 			ca=ws.cell(row=j, column=c_write).value
@@ -403,6 +404,7 @@ def A_Statu_day4(c_write,j,ResAirbnb,new_mo):
 		except:
 			break
 	ws.cell(row=j, column=c_write+1).value = str(li)
+	print(li)
 	try:
 		if len(li)>0:
 			ca=ws.cell(row=j, column=c_write).value
@@ -484,6 +486,7 @@ def A_Statu_day5(c_write,j,ResAirbnb,new_mo,g):
 	except:
 		pass
 	ws.cell(row=j, column=c_write+1).value=str(li)
+	print(li)
 	try:
 		if len(li)>0:
 			ca=ws.cell(row=j, column=c_write).value
@@ -1030,9 +1033,11 @@ while end==0:
 					p4=p3.replace('","', '')
 					print (p4)
 					if p4==V_up:
-						ResAirbnb='/A'
+						#ResAirbnb='/A'
+						ResAirbnb=''
 					else:
-						ws.cell(row=j, column=k).value=p4
+						#ws.cell(row=j, column=k).value=p4
+						ResAirbnb=''
 				except:
 					pass
 				time.sleep(1)
